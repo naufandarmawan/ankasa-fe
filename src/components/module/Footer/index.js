18273ca21e25b514bddaa7b5d161f3ea5e287623
naufandarmawan <naufandarmawan@gmail.com>
2024-08-27 10:06:40 +0700

    Add Home Page

--- src/components/module/Footer/index.js
+++ src/components/module/Footer/index.js
@@ -3,13 +3,52 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <ul className="bg-blue-50 flex gap-6 justify-center p-6 max-lg:flex-col max-lg:p-4 max-lg:gap-4 max-lg:pb-16">
-      <li>
-        <Link className="font-medium text-base text-[#707070]" href="">
-          Footer
-        </Link>
-      </li>
-    </ul>
+    <div className="px-40 py-16 flex flex-col gap-10">
+      <div className="flex gap-20">
+        <div className="flex flex-col gap-8">
+          <p>Ankasa</p>
+          <p className="font-normal text-sm text-[#6B6B6B] leading-8">
+            Find your Flight and explore the world with us. We will take care of the rest
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <p className="font-medium text-base text-black">Features</p>
+          <div className="flex flex-col gap-5">
+            <p className="font-normal text-sm text-[#6B6B6B]">Find Ticket</p>
+            <p className="font-normal text-sm text-[#6B6B6B]">My Booking</p>
+            <p className="font-normal text-sm text-[#6B6B6B]">Chat</p>
+            <p className="font-normal text-sm text-[#6B6B6B]">Notification</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <p className="font-medium text-base text-black">Download Angkasa app</p>
+          <div>
+            <p>App Store</p>
+            <p>Google Play</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <p className="font-medium text-base text-black">Follow Us</p>
+          <div className="flex gap-5">
+            <p>FB</p>
+            <p>TW</p>
+            <p>IG</p>
+            <p>YT</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center">
+        <p className="font-normal text-base text-[#6B6B6B] leading-8">© Ankasa. All Rights Reserved.</p>
+        <div className="flex gap-2 items-center">
+          <p className="font-normal text-sm text-[#595959]">Pin</p>
+          <p className="font-normal text-sm text-[#6B6B6B]">Jakarta Indonesia</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/module/Navbar/index.js
+++ src/components/module/Navbar/index.js
@@ -1,15 +1,64 @@
 import React from "react";
-import Link from "next/link";
+
+import {
+  Avatar,
+  Button,
+  Divider,
+  IconButton,
+  InputBase,
+  Paper,
+  Stack,
+} from "@mui/material";
+import { EmailOutlined, NotificationsOutlined } from "@mui/icons-material";
 
 const Navbar = () => {
   return (
-    <ul className="bg-blue-50 flex gap-6 justify-center p-6 max-lg:flex-col max-lg:p-4 max-lg:gap-4 max-lg:pb-16">
-      <li>
-        <Link className="font-medium text-base text-[#707070]" href="">
-          Navbar
-        </Link>
-      </li>
-    </ul>
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      spacing={0}
+      sx={{ minWidth: "auto" }}
+      className="px-40 py-10"
+    >
+      <p>Logo</p>
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <IconButton sx={{ p: "10px" }} aria-label="menu">
+          <EmailOutlined />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Google Maps"
+          inputProps={{ "aria-label": "search google maps" }}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <EmailOutlined />
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
+          <EmailOutlined />
+        </IconButton>
+      </Paper>
+      <Stack
+        direction="row"
+        spacing={2}
+      >
+        <Button variant="text">Text 1</Button>
+        <Button variant="text">Text 2</Button>
+      </Stack>
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+      >
+        <EmailOutlined />
+        <NotificationsOutlined />
+        <Avatar>H</Avatar>
+      </Stack>
+    </Stack>
   );
 };
 
