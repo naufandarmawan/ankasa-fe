a634a52f048f5f55b0e4b9a084291a67a443fa71
naufandarmawan <naufandarmawan@gmail.com>
2024-08-28 09:40:47 +0700

    Add Search

--- src/app/(main)/search/page.js
+++ src/app/(main)/search/page.js
@@ -1,9 +1,104 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div>
+      <div>Biru</div>
+      <div>
+        <div>
+          <div>
+            <p>Filter</p>
+            <p>Reset</p>
+          </div>
 
-export default Search
+          <div>
+            <div>
+              <div>
+                <p>Transit</p>
+                <p> = </p>
+              </div>
+              <div>
+                <div>
+                  <p>Direct</p>
+                  <p>Checkbox</p>
+                </div>
+                <div>
+                  <p>Direct</p>
+                  <p>Checkbox</p>
+                </div>
+                <div>
+                  <p>Direct</p>
+                  <p>Checkbox</p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <p>Transit</p>
+                <p> = </p>
+              </div>
+              <div>
+                <div>
+                  <p>Direct</p>
+                  <p>Checkbox</p>
+                </div>
+                <div>
+                  <p>Direct</p>
+                  <p>Checkbox</p>
+                </div>
+                <div>
+                  <p>Direct</p>
+                  <p>Checkbox</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Select Ticket</p>
+            <p>(6 flight found)</p>
+            <p>Sort by</p>
+            <p>+</p>
+          </div>
+          <div>
+            <div>
+              <p>Logo</p>
+              <p>Garuda Indonesia</p>
+            </div>
+
+            <div>
+              <div>
+                <p>IDN</p>
+                <p>12:33</p>
+                <p>+</p>
+                <p>IDN</p>
+                <p>12:33</p>
+              </div>
+
+              <div>
+                <p>3 hours 11 minutes</p>
+                <p>(1 transit)</p>
+              </div>
+
+              <div>
+                <p>+</p>
+                <p>+</p>
+                <p>+</p>
+              </div>
+
+              <p>
+                <span>$ 214,00</span>/pax
+              </p>
+
+              <p>Select</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
